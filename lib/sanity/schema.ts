51d42e00f21433b1adcd19ee96d1967ca193d3e8
exceltrade <saeeann@gmail.com>
2026-05-06 14:16:17 -0400

    Create schema.ts to export document schemas

---
+++ lib/sanity/schema.ts
@@ -0,0 +1,8 @@
+import pageHome from "./documents/pageHome";
+import service from "./documents/service";
+import caseStudy from "./documents/caseStudy";
+import siteSettings from "./documents/siteSettings";
+
+const schema = [pageHome, service, caseStudy, siteSettings];
+
+export default schema;
