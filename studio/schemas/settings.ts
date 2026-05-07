1666f4463ab86c9f2a0cefec661cc2e0454b3597
exceltrade <saeeann@gmail.com>
2026-05-07 01:08:29 -0400

    Add settings schema for site configuration

---
+++ studio/schemas/settings.ts
@@ -0,0 +1,22 @@
+export default {
+  name: 'settings',
+  title: 'Site Settings',
+  type: 'document',
+  fields: [
+    {
+      name: 'siteTitle',
+      title: 'Site Title',
+      type: 'string'
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text'
+    },
+    {
+      name: 'logo',
+      title: 'Logo',
+      type: 'image'
+    }
+  ]
+}
