186d35f9e52cb6dd9f3b68f3a79f8c39235eea2b
exceltrade <saeeann@gmail.com>
2026-05-07 01:39:34 -0400

    Switch Sanity client import to next-sanity

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,8 +1,8 @@
-import { createClient } from "@sanity/client";
+import { createClient } from 'next-sanity'
 
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: "2024-05-01",
-  useCdn: true
-});
+  useCdn: true,
+})
