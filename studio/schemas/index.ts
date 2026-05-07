63e16ab01e1ed3b44fcfe3fb0aba7e2c298f7c9b
exceltrade <saeeann@gmail.com>
2026-05-07 01:09:19 -0400

    Refactor schema index to include desk structure

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,4 +1,9 @@
-import home from './home'
-import settings from './settings'
+import {StructureBuilder} from 'sanity/desk'
 
-export const schemaTypes = [home, settings]
+export const deskStructure = (S: StructureBuilder) =>
+  S.list()
+    .title('Content')
+    .items([
+      S.documentTypeListItem('home').title('Home Page'),
+      S.documentTypeListItem('settings').title('Site Settings')
+    ])
