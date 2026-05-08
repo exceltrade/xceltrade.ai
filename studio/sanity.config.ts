33850a0f21eaf7dcb475d92af8049fc62faef14b
exceltrade <saeeann@gmail.com>
2026-05-08 14:36:51 -0400

    Update import for schema in sanity.config.ts

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,7 +1,8 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import schema from "../sanity/schema";
+
 
 export default defineConfig({
   name: 'default',
